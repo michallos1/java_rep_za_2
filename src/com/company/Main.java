46f6e7357781de17c8ca819c92c65b694872dee1
Michal Los <42137480+michallos1@users.noreply.github.com>
2020-10-13 15:53:27 +0200

    pentle

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,60 @@
 package com.company;
 
+import java.security.spec.RSAOtherPrimeInfo;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("hello");
+//        System.out.println("hello");
+//        System.out.println("hell2o");
+//
+//        int zmienna = 1;
+//        System.out.println(zmienna);
+//        long zmienna_l = 10000000000L;
+//        System.out.println(zmienna_l);
+//
+//
+//        char zmienna_znakowa = 'a';
+//        System.out.println(zmienna_znakowa);
+//
+//        String zmienna_s ="ko";
+//        System.out.println(zmienna_s);
+//
+//        System.out.println(zmienna+zmienna_l);
+//
+//        System.out.println(zmienna_s+zmienna);
+//
+//        float zmienna_zm = 2.223F;
+//        float t = zmienna_zm+zmienna;
+//
+//        System.out.println(t);
+//
+//        boolean bl = false;
+//
+//        if(zmienna_s.equals("k")){
+//            System.out.println("k");
+//        }
+//        else if(zmienna_s.equals("m")){
+//            System.out.println("m");
+//        }
+
+        int i = 1;
+
+//        while (i<10){
+//            i++;
+//            System.out.println(i);
+//        }
+
+//        do {
+//
+//            i++;
+//            System.out.println(i);
+//        }while (i<10);
+
+        for (i=0; i<=10; i++){
+            System.out.println(i);
+        }
+
 
     }
 }
